e0a29db6280e1c106365167eacee87b74322e4b4
Jimmy Puckett <jimmy.puckett@spinen.com>
2017-07-09 19:43:47 -0400

    Mock out the router on the test

--- tests/SsoServiceProviderTest.php
+++ tests/SsoServiceProviderTest.php
@@ -3,6 +3,7 @@
 namespace Spinen\Discourse;
 
 use ArrayAccess as Application;
+use Illuminate\Contracts\Routing\Registrar as Router;
 use Illuminate\Support\ServiceProvider;
 use Mockery;
 
@@ -24,6 +25,11 @@ class SsoServiceProviderTest extends TestCase
     protected $purge_command_mock;
 
     /**
+     * @var Mockery\Mock
+     */
+    protected $router_mock;
+
+    /**
      * @var ServiceProvider
      */
     protected $service_provider;
@@ -40,6 +46,7 @@ class SsoServiceProviderTest extends TestCase
     private function setUpMocks()
     {
         $this->application_mock = Mockery::mock(Application::class);
+        $this->router_mock = Mockery::mock(Router::class);
     }
 
     /**
@@ -55,6 +62,16 @@ class SsoServiceProviderTest extends TestCase
      */
     public function it_boots_the_service()
     {
+        $this->application_mock->shouldReceive('offsetGet')
+                               ->once()
+                               ->with('router')
+                               ->andReturn($this->router_mock);
+
+        $this->router_mock->shouldReceive('group')
+                          ->once()
+                          ->withArgs([["middleware" => "auth"], Mockery::any()]);
+
+        // TODO: Make sure that when the closure is called as expected
         $this->assertNull($this->service_provider->boot());
     }
 }
